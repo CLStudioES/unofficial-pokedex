91676c45657c84066b8aa14d295f2e366ffab3b5
CLStudio <karlosky12@gmail.com>
2024-04-09 16:29:55 +0200

    Seguimos

--- src/consts/pokemonG1.js
+++ src/consts/pokemonG1.js
@@ -2,7 +2,7 @@ let response;
 let data;
 export let pokemonListG1 = [];
 
-for (let index = 1; index < 151; index++) {
+for (let index = 1; index <= 151; index++) {
     response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${index}`,
     );
