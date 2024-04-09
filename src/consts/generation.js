d1fc49e6e971f89579d8ea84a32067dbd9f87d03
Carlos <karlosky12@gmail.com>
2024-04-09 21:52:14 +0200

    Conseguimos linkar id, nombre, sprite y tipo1

---
+++ src/consts/generation.js
@@ -0,0 +1,43 @@
+export let firstPokemon = 1;
+export let lastPokemon = 1025;
+
+function selectG1() {
+    firtPokemon = 1;
+    lastPokemon = 151;
+}
+function selectG2() {
+    firtPokemon = 152;
+    lastPokemon = 251;
+}
+function selectG3() {
+    firtPokemon = 252;
+    lastPokemon = 386;
+}
+function selectG4() {
+    firtPokemon = 387;
+    lastPokemon = 493;
+}
+function selectG5() {
+    firtPokemon = 494;
+    lastPokemon = 649;
+}
+function selectG6() {
+    firtPokemon = 650;
+    lastPokemon = 721;
+}
+function selectG7() {
+    firtPokemon = 722;
+    lastPokemon = 809;
+}
+function selectG8() {
+    firtPokemon = 810;
+    lastPokemon = 905;
+}
+function selectG9() {
+    firtPokemon = 906;
+    lastPokemon = 1025;
+}
+function selectAll() {
+    firtPokemon = 1;
+    lastPokemon = 1025;
+}
--- src/consts/pokemonList.js
+++ src/consts/pokemonList.js
@@ -1,13 +1,11 @@
-
 let response;
 let data;
-let pokemonList = [];
+export let pokemonList = [];
 
-for (let index = 152; index < 251; index++) {
+for (let index = 1; index <= 151; index++) {
     response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${index}`,
     );
     data = await response.json();
     pokemonList[index - 1] = data;
 }
-return pokemonList;
