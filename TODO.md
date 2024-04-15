354a4e672b34004ac0ace11bbd7bc2dabca7f2e6
Carlos <karlosky12@gmail.com>
2024-04-15 21:45:42 +0200

    Cambio de generaciones con el header

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
-- []    Buttons action cambio entre generaciones
+- [X]    Buttons action cambio entre generaciones
 - []    Añadir spinner para la carga
 - []    Load de 20 imágenes y cargar siguientes
-- []    Revisión deiseño y optimización cards
+- []    Revisión de diseño y optimización cards
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,46 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  routes: [{
+    path: '/index',
+    handler: () => import('./pages/index.astro')
+  },
+  {
+    path: '/Generation1',
+    handler: () => import('./pages/generation1.astro')
+  },
+  {
+    path: '/Generation12',
+    handler: () => import('./pages/generation2.astro')
+  },
+  {
+    path: '/Generation3',
+    handler: () => import('./pages/generation3.astro')
+  },
+  {
+    path: '/Generation4',
+    handler: () => import('./pages/generation4.astro')
+  },
+  {
+    path: '/Generation5',
+    handler: () => import('./pages/generation5.astro')
+  },
+  {
+    path: '/Generation6',
+    handler: () => import('./pages/generation6.astro')
+  },
+  {
+    path: '/Generation7',
+    handler: () => import('./pages/generation7.astro')
+  },
+  {
+    path: '/Generation8',
+    handler: () => import('./pages/generation8.astro')
+  },
+  {
+    path: '/Generation9',
+    handler: () => import('./pages/generation9.astro')
+  },
+  ]
 });
