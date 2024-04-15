d5f91739acc3851da426fdee2ae75628c39102a1
CLStudio <karlosky12@gmail.com>
2024-04-15 16:03:00 +0200

    Falta cambiar generación con un click

--- src/consts/pokemonList.js
+++ src/consts/pokemonList.js
@@ -12,7 +12,7 @@ let pokemonListG9 = [];
 
 //Gen 1
 export async function getG1() {
-    console.log("Seleccionada G1");
+    //console.log("Seleccionada G1");
     for (let index = 1; index <= 151; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -25,7 +25,7 @@ export async function getG1() {
 
 //Gen 2
 export async function getG2() {
-    console.log("Seleccionada G2");
+    //console.log("Seleccionada G2");
     for (let index = 152; index <= 251; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -37,7 +37,7 @@ export async function getG2() {
 }
 //Gen 3
 export async function getG3() {
-    console.log("Seleccionada G3");
+    //console.log("Seleccionada G3");
     for (let index = 252; index <= 386; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -49,7 +49,7 @@ export async function getG3() {
 }
 //Gen 4
 export async function getG4() {
-    console.log("Seleccionada G4");
+    //console.log("Seleccionada G4");
     for (let index = 387; index <= 493; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -61,7 +61,7 @@ export async function getG4() {
 }
 //Gen 5
 export async function getG5() {
-    console.log("Seleccionada G5");
+    //console.log("Seleccionada G5");
     for (let index = 494; index <= 649; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -73,7 +73,7 @@ export async function getG5() {
 }
 //Gen 6
 export async function getG6() {
-    console.log("Seleccionada G6");
+    //console.log("Seleccionada G6");
     for (let index = 650; index <= 721; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -85,7 +85,7 @@ export async function getG6() {
 }
 //Gen 7
 export async function getG7() {
-    console.log("Seleccionada G7");
+    //console.log("Seleccionada G7");
     for (let index = 722; index <= 809; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -97,7 +97,7 @@ export async function getG7() {
 }
 //Gen 8
 export async function getG8() {
-    console.log("Seleccionada G8");
+    //console.log("Seleccionada G8");
     for (let index = 810; index <= 905; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
@@ -109,7 +109,7 @@ export async function getG8() {
 }
 //Gen 9
 export async function getG9() {
-    console.log("Seleccionada G9");
+    //console.log("Seleccionada G9");
     for (let index = 906; index <= 1025; index++) {
         response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${index}`,
